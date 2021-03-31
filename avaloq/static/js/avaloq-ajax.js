946e26cff0881b9ac2cf4f7272279eddd55e6b36
James <2251463r@student.gla.ac.uk>
2021-03-31 11:31:33 +0100

    Bug fix V1.2.2 Fixed delete button bug

--- avaloq/static/js/avaloq-ajax.js
+++ avaloq/static/js/avaloq-ajax.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
 
-    $('#delete_btn').click(function() {
+    $('.delete_btn').click(function() {
         if (confirm("Once a candidate is deleted their submission cannot be recovered. Are you sure?") == true) {
 
 
-            var candidateID = $(this).attr('data-candidateid');
+            var candidateID = this.id;
             $.get('/avaloq/delete_candidate', { 'u_id': candidateID },
                 function(data) {
                     location.reload();
